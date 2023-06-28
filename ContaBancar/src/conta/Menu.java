69c50be59dd741c85080ce2cd1d32744105ab803
Rillary <94876655+RillaryDev@users.noreply.github.com>
2023-06-28 00:07:40 -0300

    Task 03

--- ContaBancar/src/conta/Menu.java
+++ ContaBancar/src/conta/Menu.java
@@ -1,15 +1,46 @@
 package conta;
+
 import java.util.Scanner;
 
-import conta.Cores;
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
+import util.Cores;
 
 public class Menu {
 
 		public static void main(String[] args) {
-
+			
+			int opcao;
+			
 			Scanner sc = new Scanner(System.in);
 
-			int opcao;
+
+			Conta c1 = new Conta(1, 123, 1, "Morgana", 10000.0f);
+			c1.visualizar();
+			c1.sacar(12000.0f);
+			c1.visualizar();
+			c1.depositar(5000.0f);
+			c1.visualizar();
+
+
+			// Classe Conta Corrente
+			ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "Rillary Corrêa", 0.0f, 1000.0f);
+			cc1.visualizar();
+			cc1.sacar(12000.0f);
+			cc1.visualizar();
+			cc1.depositar(5000.0f);
+			cc1.visualizar();
+			
+	        // Classe Conta Poupança
+			ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Elise Ferreira", 100000.0f, 15);
+			cp1.visualizar();
+	        cp1.sacar(1000.0f);
+			cp1.visualizar();
+			cp1.depositar(5000.0f);
+			cp1.visualizar();
+			
+
 
 			while (true) {
 				
